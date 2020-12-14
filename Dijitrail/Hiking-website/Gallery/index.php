25c6574742f81dbe965ae92f0ab0ac0f750e96ea
dylanfarrell01 <56674360+dylanfarrell01@users.noreply.github.com>
2020-12-14 01:49:21 +0000

    Update index.php

--- Dijitrail/Hiking-website/Gallery/index.php
+++ Dijitrail/Hiking-website/Gallery/index.php
@@ -7,10 +7,14 @@
             </div>
             <div class = "row">
                 <div class = "column">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../Gallery//Rating1.html">
                     <img src = "Connacht.png" alt = "West" title = "West" class = "provinces">
+                        </a>
                 </div>
                 <div class = "column">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../Gallery//Rating1.html">
                     <img src = "Leinster.png" alt = "East" title = "East" class = "provinces">
+                        </a>
                 </div>
             </div>
             <div class = "outerDiv">
@@ -19,10 +23,14 @@
             </div>
             <div class = "row">
                 <div class = "column">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../Gallery//Rating1.html">
                     <img src = "Ulster.png" alt = "North" title = "North" class = "provinces">
+                        </a>
                 </div>
                 <div class = "column">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../Gallery//Rating1.html">
                     <img src = "Munster.png" alt = "South" title = "South" class = "provinces">
+                        </a>
                 </div>
             </div>
         </div>
